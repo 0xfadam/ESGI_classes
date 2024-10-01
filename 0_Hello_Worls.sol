ec194932b847126b273bcf6cb555796aa113b692
0xfadam <60340419+0xfadam@users.noreply.github.com>
2024-10-01 11:39:45 +0200

    Update 0_Hello_Worls.sol

--- 0_Hello_Worls.sol
+++ 0_Hello_Worls.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+contract HelloWorld {
+
+  string data;
+
+  constructor(){
+  }
+
+    function sayHellow() public view returns (string memory) {
+        return data;
+    }
+
+  function changeData(string memory newString) public {
+      data=newString;
+  }
+}
